64693e49b62ada2d7775f4999a0dbf36daa6fa4b
Soulmate13 <glebzhurba@gmail.com>
2020-01-02 14:34:50 +0200

    add favicon

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@ import sliders from './sliders';
 import AOS from 'aos';
 import burger from './burger';
 import map from './map';
+import modernizr from './modernizr-custom';
+import 'lazysizes';
+
 AOS.init();
 
 
@@ -9,4 +12,5 @@ $(document).ready(function () {
     sliders();
     burger();
     map();
+    modernizr();
 });
